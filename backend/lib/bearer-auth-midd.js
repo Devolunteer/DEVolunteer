433c08b9d191aa1ee5b354708a83f7ff4de9ce90
Frank Iriarte <jurrish@gmail.com>
2017-03-26 11:46:12 -0700

    bearer auth written

--- backend/lib/bearer-auth-midd.js
+++ backend/lib/bearer-auth-midd.js
@@ -1,7 +1,27 @@
 'use strict'
 
 const jwt = require('jsonwebtoken')
+const User = require('../model/user')
 const createError = require('http-errors')
 
 //findHash prop is on dev schema
-const dev = 
+
+module.exports = (req, res, next) => {
+  let authHeader = req.headers.authorization
+  if(!authHeader) return next(createError(401, 'requires header'))
+  if(!token) return next(createError(401, 'requires token'))
+
+  let token = authHeader.split('Bearer')[1]
+
+  jwt.verify(token, process.env.SECRET || 'DEV', (err, decodedToken) => {
+    if(err) return next(createError(500, 'server error'))
+    User.findOne({findHash: decodedToken})
+    .then(User => {
+      req.user = User
+      next()
+    })
+    .catch(() => {
+      next(() => createError(401, 'token creation failed'))
+    })
+  })
+}
