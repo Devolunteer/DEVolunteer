140c95a94141df6a7ce6eeb6f4a54ec112f2dc67
jisenber <isenberg.jacob@yahoo.com>
2017-03-28 00:05:24 -0700

    views added, better structure implemented

---
+++ app/view/loginview/loginview-controller.js
@@ -0,0 +1,10 @@
+'use strict';
+
+require('./_loginview.scss');
+
+
+module.exports = ['$log', LoginviewController];
+
+function LoginviewController($log) {
+  $log.debug('In the login view controller');
+}
---
+++ app/view/signupview/signupview-controller.js
@@ -0,0 +1,10 @@
+'use strict';
+
+require('./_signupview.scss');
+
+
+module.exports = ['$log', SignupviewController];
+
+function SignupviewController($log) {
+  $log.debug('In the signup-view controller');
+}
