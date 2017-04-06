a86e459677628fa278681668d243747b8e444df5
Michael Bishop <polishthebishop@gmail.com>
2017-04-06 14:26:11 -0700

    better styling

--- app/view/about-us/about-us.js
+++ app/view/about-us/about-us.js
@@ -1,8 +1,37 @@
+// 'use strict';
+//
+// console.log('in the controller');
+// require('./_about-us.scss');
+//
+// require('angular').module('DEVolunteer')
+// .component('aboutUs', {
+//   template: require('./about-us.html'),
+//   controllerAs: 'aboutUsCtrl',
+//   controller: [function() {
+//     console.log('booyah');
+//     this.$onInit = function() {
+//       this.imageURI = require('../../assets/img/coders/me.png');
+//       console.log('this is the image', this.imageURI);
+//     }
+//   }]
+// });
+
 'use strict';
 
 require('./_about-us.scss');
 
-require('angular').module('DEVolunteer')
-.component('aboutUs', {
+
+module.exports = {
   template: require('./about-us.html'),
-});
+  controller: ['$log', AboutUsController],
+  controllerAs: 'aboutUsCtrl',
+};
+
+function AboutUsController($log) {
+  $log.debug('AboutUs Controller');
+
+  this.$onInit = function() {
+    this.imgURI = require('../../assets/img/coders/me.png');
+    console.log(this.imgURI);
+  }
+}
