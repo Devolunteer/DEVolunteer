f9680f7494b8549f599ee0875bc6295d88c6bd00
spamalope01 <spamalope01@yahoo.com>
2017-04-08 15:32:37 -0700

    tweaked stylings here and there.  added the tiles on landing.

--- app/view/partners/partners-controller.js
+++ app/view/partners/partners-controller.js
@@ -8,23 +8,3 @@ module.exports = ['$log', PartnersController];
 function PartnersController($log) {
   $log.debug('In the partners controller');
 }
-
-
-  // THIS WILL NEED TO PULL IN OR HAVE THE INFORMATION FOR THE QUESTIONAIRE TO DISPLAY.
-
-
-  // this.today = new Date();
-  // this.galleries = [];
-  // console.log('this.galleries', this.galleries);
-  //
-  // this.showQuestions = function(){
-  //   questService.showQuest()
-  //   .then(galleries => {
-  //     this.galleries = galleries;
-  //     console.log('this.galleries2', this.galleries);
-  //   });
-  // };
-
-  // $rootScope.$on('$locationChangeSuccess', () =>{
-  //   this.fetchGalleries();
-  // })
